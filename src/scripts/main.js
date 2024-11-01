c261aa5fd4655d6943c8e8b663904ec7059dd5c5
Roman Rusin <rgromkorusin12@gmail.com>
2024-11-01 16:50:05 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,26 @@
 'use strict';
 
-function sortList(list) {
-  let myList = list;
+// function sortList(list) {
+//   const salaryElement = list.querySelector('[data-salary]');
+// }
 
-  myList = document.querySelector('ul');
-
-  const salaryElement = myList.querySelector('data-salary');
-
-  return salaryElement;
+function parseSalary(salaryString) {
+  return Number(salaryString);
 }
 
-sortList();
+function sortList(list) {
+  const itemsArray = Array.from(list.children);
+
+  itemsArray.sort((a, b) => {
+    const salaryA = parseSalary(a.getAttribute('data-salary'));
+    const salaryB = parseSalary(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+
+  itemsArray.forEach((item) => list.appendChild(item));
+}
+
+const myList = document.querySelector('ul');
+
+sortList(myList);
